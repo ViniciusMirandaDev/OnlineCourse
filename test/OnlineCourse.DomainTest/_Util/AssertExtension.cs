fb52331a8153f615916d58919bf21b5d9041a9b0
ViniciusMirandaDev <vinimb07@gmail.com>
2023-03-12 21:19:45 -0300

    feat: create util class to assert exception message

---
+++ test/OnlineCourse.DomainTest/_Util/AssertExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineCourse.DomainTest._Util
+{
+    public static class AssertExtension
+    {
+        public static void WithMessage(this ArgumentException ex, string message)
+        {
+            if (ex.Message == message)
+                Assert.True(true);
+            else
+                Assert.False(true, "Expected message:" + message);
+        }
+    }
+}
